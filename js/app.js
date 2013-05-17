732ca71b1108c664c6fbd6311bdf3986784f1cb2
William Estoque <william.estoque@gmail.com>
2013-05-17 15:56:55 -0700

    Added edit view.

--- js/app.js
+++ js/app.js
@@ -2,34 +2,56 @@ App = Ember.Application.create({});
 
 App.Store = DS.Store.extend({
   revision: 12,
-  adapter: DS.RESTAdapter.extend({
-    url: 'http://localhost:3000'
+  adapter: DS.LSAdapter.create({
+    namespace: 'bloggr-client'
   })
 });
 
 App.Router.map(function() {
   this.resource('about');
   this.resource('posts', function() {
-    this.resource('post', { path: ':post_id' });
+    this.resource('post', {path: ':post_id'}, function() {
+      this.route('edit');
+    });
   });
+});
+
+App.PostEditRoute = Ember.Route.extend({
+
+  model: function(params) {
+    return this.modelFor('post');
+  }
+
+});
+
+App.PostEditController = Ember.ObjectController.extend({
+
+  doneEditing: function() {
+    this.get('store').commit();
+    this.transitionToRoute('post', this.content);
+  }
+
+});
+
+App.PostIndexRoute = Ember.Route.extend({
+
+  model: function(params) {
+    return this.modelFor('post');
+  }
+
+});
+
+App.IndexRoute = Ember.Route.extend({
+
+  redirect: function() {
+    this.transitionTo('posts');
+  }
+
 });
 
 App.PostsRoute = Ember.Route.extend({
   model: function() {
     return App.Post.find();
-  }
-});
-
-App.PostController = Ember.ObjectController.extend({
-  isEditing: false,
-
-  edit: function() {
-    this.set('isEditing', true);
-  },
-
-  doneEditing: function() {
-    this.set('isEditing', false);
-    this.get('store').commit();
   }
 });
 
@@ -42,6 +64,15 @@ App.Post = DS.Model.extend({
   extended: attr('string'),
   publishedAt: attr('date')
 });
+
+//App.Post.FIXTURES = [{
+  //id: 1,
+  //title: 'The Sexy Beast',
+  //author: 'Dan Cohen',
+  //intro: 'Whats up friends!',
+  //extended: 'Yoyoyo!',
+  //publishedAt: new Date()
+//}];
 
 var showdown = new Showdown.converter();
 
